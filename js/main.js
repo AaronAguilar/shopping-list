31c88da997e3d31c6584b0bfca2236c44162c6f7
aaronaguilar <aguilarh.aaron@gmail.com>
2015-08-25 17:19:55 -0700

    got it working.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 //this app will take user input and display it on the page
 
+    /*
+
     var theButton = document.querySelector('.btn');
     
     var theField = document.querySelector('.inputfields');
@@ -20,32 +22,33 @@
 
     var list = document.querySelector('ul');
     
+    */
 
-// append it to the empty <ul>
+    // append it to the empty <ul>
 
-//
+    // display on the body
 
-// display on the body
+    var theButton = document.querySelector('.btn');
 
-var theButton = document.querySelector('.btn');
+    //grab the ul from the html
+    
+    var list = document.querySelector('ul');
 
-console.log('yo');
-//grab the ul from the html
-var list = document.querySelector('ul');
+    theButton.addEventListener('click', addItem, false);
 
-theButton.addEventListener('click', addItem, false);
-
-function addItem(){
- // grab the input value and put it in a variable 
- var theItem = document.querySelector('#inputItem').value;
+    function addItem() {
+    
+    // grab the input value and put it in a variable 
+    var theItem = document.querySelector('#inputItem').value;
  
- //create new li element to append to ul
- var addLi = document.createElement('li');
+    //create new li element to append to ul
+    var addLi = document.createElement('li');
  
- //add input value to li
- addLi.textContent = theItem;
+    //add input value to li
+    addLi.textContent = theItem;
  
- //add new li to list (ul)
- list.appendChild(addLi);
- console.log('clicked the button');
-   }
+    //add new li to list (ul)
+    list.appendChild(addLi);
+    console.log('clicked the button');
+    
+}
